93dba604761ab87f0f7aa259a2b5936102cf5db6
f.estacio@gmail.com <f.estacio@gmail.com>
2020-08-07 12:32:40 +0100

    completed the animation using keyframes

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,26 +3,55 @@ import styled from 'styled-components';
 import { Square } from './Square';
 
 const StyledGrid = styled.div`
-  margin: auto;
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-gap: 5px;
+  grid-template-columns: ${(props) => `repeat(4, ${props.squareSize})`};
   grid-template-rows: auto auto;
-  grid-column-gap: 20px;
-  grid-row-gap: 20px;
-  justify-content: center;
 `;
 
-export const Grid = () => {
+export const Grid = ({ squareSize = '200px' }) => {
   return (
-    <StyledGrid>
-      <Square size="300px">Hello!</Square>
-      <Square size="150px"></Square>
-      <Square caption={{ title: 'hello', text: 'Lorem' }}></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
+    <StyledGrid squareSize={squareSize}>
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div1', text: 'caption1' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div2', text: 'caption2' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div3', text: 'caption3' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div4', text: 'caption4' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div5', text: 'caption5' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div6', text: 'caption6' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div7', text: 'caption7' }}
+      />
+      <Square
+        size={squareSize}
+        src={'https://via.placeholder.com/200'}
+        caption={{ title: 'div8', text: 'caption8' }}
+      />
     </StyledGrid>
   );
 };
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,34 +1,110 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { animated, useSpring } from 'react-spring';
+import styled, { keyframes, css } from 'styled-components';
 
-const StyledSquare = styled.div`
+const motion = {};
+
+motion.inTop = keyframes`0% {height: 0; bottom: 100%;} 100% { height: 100%; bottom: 0;}`;
+motion.inRight = keyframes`0% {width: 0; left: 100%;} 100% {width: 100%; left: 0;}`;
+motion.inBottom = keyframes`0% {height: 0;} 100% {height: 100%;}`;
+motion.inLeft = keyframes`0% {width: 0;} 100% {width: 100%;}`;
+motion.outTop = keyframes`0% {height: 100%; bottom: 0;} 100% {height: 0; bottom: 100%}`;
+motion.outRight = keyframes`0% {width: 100%; left: 0;} 100% {width: 0; left: 100%;}`;
+motion.outBottom = keyframes`0% {height: 100%;} 100% {height: 0;}`;
+motion.outLeft = keyframes`0% {width: 100%;} 100% {width: 0;}`;
+
+const getMotion = (action, direction) => {
+  switch (action) {
+    case 'enter':
+      switch (direction) {
+        case 'top':
+          return motion.inTop;
+        case 'right':
+          return motion.inRight;
+        case 'bottom':
+          return motion.inBottom;
+        case 'left':
+          return motion.inLeft;
+        default:
+          return '';
+      }
+    case 'leave':
+      switch (direction) {
+        case 'top':
+          return motion.outTop;
+        case 'right':
+          return motion.outRight;
+        case 'bottom':
+          return motion.outBottom;
+        case 'left':
+          return motion.outLeft;
+        default:
+          return '';
+      }
+    default:
+      return '';
+  }
+};
+
+const animation = ({ action, direction }) => css`
+  ${getMotion(action, direction)} 0.2s ease forwards;
+`;
+
+const Container = styled.div`
+  position: relative;
   width: ${(props) => props.size || '200px'};
   height: ${(props) => props.size || '200px'};
-  border: 1px solid black;
-  justify-self: center;
-  align-self: center;
-  background: red;
-  perspective: 400px;
 `;
 
-const TextBox = styled(animated.div)`
+const Image = styled.img`
+  display: block;
   width: 100%;
-  height: 100%;
-  background: green;
-  position: absolute;
-  top: 0;
-  left: 0;
-  border-radius: 4px;
-  pointer-events: none;
-  h3,
-  p {
-    margin: 0;
-  }
+  height: auto;
 `;
 
-export const Square = ({ size, caption = {} }) => {
-  const [direction, setDirection] = useState();
+const Overlay = styled.div`
+  position: absolute;
+  background-color: #008cba;
+  overflow: hidden;
+  height: 0;
+  width: 0;
+  ${(props) => props.initialPosition}
+  animation: ${animation};
+`;
+
+const Text = styled.div`
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+export const Square = ({ size, caption = {}, src }) => {
+  const [direction, setDirection] = useState('');
+  const [action, setAction] = useState('');
+
+  const handleMouseEnter = (event) => {
+    setDirection(getDirection(event));
+    setAction(getAction(event));
+  };
+
+  const handleMouseLeave = (event) => {
+    setDirection(getDirection(event));
+    setAction(getAction(event));
+  };
+
+  const getAction = (event) => {
+    switch (event.type) {
+      case 'mouseleave':
+        return 'leave';
+      case 'mouseenter':
+        return 'enter';
+      default:
+        return '';
+    }
+  };
 
   const getDirection = (event) => {
     const item = event.currentTarget;
@@ -51,36 +127,45 @@ export const Square = ({ size, caption = {} }) => {
       : Math.abs(theta) <= 45
       ? 'bottom'
       : theta > 0
-      ? 'left'
-      : theta < 0
       ? 'right'
+      : theta < 0
+      ? 'left'
       : null;
   };
 
-  const mouseAction = (event) => {
-    setDirection(`${event.type}-${getDirection(event)}`);
+  const getInitialPosition = (direction) => {
+    switch (direction) {
+      case 'top':
+        return `bottom: 100%; left: 0; right: 0; width: 100%; height: 0;`;
+      case 'right':
+        return `bottom: 0; left: 100%; right: 0; width: 0; height: 100%;`;
+      case 'bottom':
+        return `bottom: 0; left: 0; right: 0; width: 100%; height: 0;`;
+      case 'left':
+        return 'bottom: 0; left: 0; right: 0; width: 0; height: 100%;';
+      default:
+        return '';
+    }
   };
 
-  const motion = useSpring({
-    opacity:
-      direction === 'mouseenter-top' ||
-      direction === 'mouseenter-right' ||
-      direction === 'mouseenter-bottom' ||
-      direction === 'mouseenter-left'
-        ? 1
-        : 0,
-  });
-
   return (
-    <StyledSquare
+    <Container
       size={size}
-      onMouseEnter={(event) => mouseAction(event)}
-      onMouseLeave={(event) => mouseAction(event)}
+      src={src}
+      onMouseEnter={(event) => handleMouseEnter(event)}
+      onMouseLeave={(event) => handleMouseLeave(event)}
     >
-      <TextBox style={motion}>
-        <h3>{caption.title}</h3>
-        <p>{caption.text}</p>
-      </TextBox>
-    </StyledSquare>
+      <Image src={src}></Image>
+      <Overlay
+        initialPosition={getInitialPosition(direction)}
+        action={action}
+        direction={direction}
+      >
+        <Text>
+          {caption.title && <h3>{caption.title}</h3>}
+          {caption.text && <p>{caption.text}</p>}
+        </Text>
+      </Overlay>
+    </Container>
   );
 };
